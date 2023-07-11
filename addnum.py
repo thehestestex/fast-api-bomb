054b3f5fcc551b82d5d1dfef49b8ab3029ced169
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-07-11 13:26:19 +0530

    Update addnum.py

--- addnum.py
+++ addnum.py
@@ -1,15 +1,29 @@
 from pymongo.mongo_client import MongoClient
+import asyncio
 conn = MongoClient("mongodb+srv://jatinkalwar:shifaanam@mbomb.ghtntua.mongodb.net")
 
 
-def addnumm(mobn , unam):
+async def addnumm(mobn , unam):
+    print("uooo")
     try:
         return conn.masbom.protect.insert_one({"status": "true", "mob": mobn , "admin": "no" ,"name": unam })
     except Exception as e:
         return {"status": "failed"}
 
-def unban(mobn):
+async def unban(mobn):
     try:
-         return conn.masbom.protect.update_one({ "mob": int(mobn) }, { '$set':  { 'status': "false" }})
+         conn.masbom.protect.update_one({ "mob": (mobn) }, { '$set':  { 'status': "false" }})
+         return {"status": "ok"}
     except Exception as e:
-        return {"status": "failedd"}
+        return {"status": "failed"}
+
+async def verifyfind(mobn):
+    jo =conn.masbom.protect.find_one({"mob": mobn} , {'mob': 1 , 'status': 1 , '_id': 0})
+    return jo
+
+async def passret(mobn):
+    try:
+        pas = conn.masbom.protect.find_one({"mob": mobn} , {'password': 1 , '_id': 0})
+        return pas['password']
+    except Exception as e:
+         return "server error"
