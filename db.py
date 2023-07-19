152e5f9843c0221b0d3c0cbe6cc7c41d212dde9a
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-07-19 20:49:51 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,24 @@
+from pymongo.mongo_client import MongoClient
+import asyncio
+conn = MongoClient("mongodb+srv://jatinkalwar:shifaanam@mbomb.ghtntua.mongodb.net")
+async def serverf():
+    try:
+        serv = conn.masbom.serverss.find_one({"pos": "maintain"}, {'server': 1, '_id': 0})
+        return (serv['server'])
+    except Exception as e:
+        return {"status: Control Server Error"}
+
+async def accessk():
+    try:
+        serr = conn.masbom.serverss.find_one({"pos": "maintain"}, {'access': 1, '_id': 0})
+        return (serr['access'])
+    except Exception as e:
+        return {"status: Control Server Error"}
+
+async def adminacc():
+    try:
+        return conn.masbom.serverss.find_one({"pos": "adminskey"}, {'bombkey': 1, '_id': 0})
+    except Exception as e:
+        return {"status: Control Server Error"}
+
+
