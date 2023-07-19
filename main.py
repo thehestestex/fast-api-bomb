b3a2f2e0d893bb1f203f91ca0a14af30fd0d2b8a
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-07-19 20:39:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,37 +30,44 @@ async def verify(mobn):
         except Exception as e:
             return { "status": "Server Error", "mob": mobn }
 
-@app.get("/num/add/{mobn}/{unam}/{passs}")
-async def nnew(mobn ,unam , passs):
-        if (len(mobn) != 10):
-            return {"status": "failed" , "reason":"10 Digit"}
-        elif ((mobn.isnumeric()) == False):
-            return {"status": "failed", "reason": "only integer allowed", "mob": mobn}
-        elif (len(passs)>6):
-            return {"status": "failed", "reason": "maximum 5 digit password", "mob": mobn}
+@app.get("/num/add/{mobn}/{unam}/")
+async def nnew(mobn ,unam , passs , key):
+        if key==None:
+            return {"status": "failed" , "reason": "no key found"}
         else:
-            def check(mobn , passs):
-                out = conn.masbom.protect.find_one({"mob": mobn}, {'status': 1})
-                if (out!=None):
-                    dat = out['status']
+            chkey = await checkkeyexits(key)
+            if chkey==None:
+                return {"status": "failed" , "reason": "Enter valid key"}
+            else:
+                if (len(mobn) != 10):
+                    return {"status": "failed" , "reason":"10 Digit"}
+                elif ((mobn.isnumeric()) == False):
+                    return {"status": "failed", "reason": "only integer allowed", "mob": mobn}
+                elif (len(passs)>6):
+                    return {"status": "failed", "reason": "maximum 5 digit password", "mob": mobn}
                 else:
-                    dat="not"
-                if (dat=="false"):
-                    try:
-                        conn.masbom.protect.update_one({ "mob": mobn }, { "$set":  { 'status': "true" , 'password': passs}})
-                        return {"status:": "ok"}
-                    except Exception as e:
-                        return {"status": "failed"}
-                elif(dat=="not"):
-                    adi = conn.masbom.protect.insert_one({"status": "true", "mob": mobn , "admin": "no" ,"name": unam , "password": passs})
-                    if(adi!=None):
-                        return {"status": "ok"}
-                    else:
-                        return {"status": "failed"}
-                else:
-                    return {"status": "already protected" , "mob": mobn}
-            ret = check(mobn , passs)
-            return ret
+                    async def check(mobn , passs):
+                        out = conn.masbom.protect.find_one({"mob": mobn}, {'status': 1})
+                        if (out!=None):
+                            dat = out['status']
+                        else:
+                            dat="not"
+                        if (dat=="false"):
+                            try:
+                                conn.masbom.protect.update_one({ "mob": mobn }, { "$set":  { 'status': "true" , 'password': passs}})
+                                return {"status:": "ok"}
+                            except Exception as e:
+                                return {"status": "failed"}
+                        elif(dat=="not"):
+                            adi = conn.masbom.protect.insert_one({"status": "true", "mob": mobn , "admin": "no" ,"name": unam , "password": passs ,"key": key})
+                            if(adi!=None):
+                                return {"status": "ok"}
+                            else:
+                                return {"status": "failed"}
+                        else:
+                            return {"status": "already protected" , "mob": mobn}
+                    ret =await check(mobn , passs)
+                    return ret
 
 
 @app.get("/num/unban/{mobn}/{key}")
@@ -87,17 +94,71 @@ async def unbid(mobn , key):
         else:
             return {"status": "Wrong key"}
 
-@app.get("/bomb/smss/{tarnumm}")
-async def bombb(tarnumm):
+@app.get("/bomber/sms/{coun}/{tarnumm}/{key}")
+async def bombb( coun , tarnumm , key):
+    ip="154.25.625.85"
     try:
-        await indsms(tarnumm)
-        return {"status":"ok"}
+        sk = await loo(ip)
+        skey = sk['sec']
+        ser= await serverf()
+        if ser=="on":
+            acce= await accessk()
+            adminacce = await adminacc()
+            if (acce==key or key==adminacce):
+                if coun=="91":
+                    ins = await indsms(tarnumm , skey)
+                    print(ins)
+                    return {"status": "ok", "reason": "Success"}
+                else:
+                    return {"status": "failed", "reason": "Country is not Supported"}
+            else:
+                return {"status": "failed" , "reason": "Wrong Access Key"}
+        else:
+            return {"status": "failed" , "reason": "server off"}
     except Exception as e:
-        print(e)
-        
+        return {"status": "failed" , "reason": "Internal server error"}
+@app.get("/starr/")
+async def star(key: str , code: str):
+    print(key, code)
+    return (await randomv())
+
 @app.get("/")
+# async def mains():
+#     htmlres= await htmll()
+#     return HTMLResponse(content=htmlres , status_code=200)
+
 def read_root( request: Request):
-    item_id="njn"
-    client_host = request.client.host
-    print(client_host)
-    return {"item_id": item_id}
+    j=2
+    conn.masbom.protect.update_one({"mob": "9990212082"}, {'$set': {'status': "false" , f"mob{j}": "9999999999"}})
+
+@app.get("/loginn/")
+async def loginn(request: Request , keyss):
+    ip = request.client.host
+    log = await loo(ip)
+    if log=="no":
+        seclog = await afterlog(ip , keyss)
+        if seclog=="ok":
+            return {"status": "ok"}
+        else:
+            return {"status": "failed"}
+
+@app.get("/firstlogin/")
+async def firstlogin(request: Request , name , keys):
+    ip = request.client.host
+    if keys==None:
+        jk = await inner(ip , name)
+        return jk
+    else:
+        jj = await checkacckey(keys , ip , name)
+        return jj
+
+        # skey = chec['sec']
+        # no = chec['no']
+        # kk = await adnewip(ip ,skey , no )
+        # if kk=="ok":
+        #     return "ok"
+        # else:
+        #     return "failed"
+
+
+
