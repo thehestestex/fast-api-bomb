0778861af64121b710dd45afa2b0016575cedd66
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-07-26 23:05:29 +0530

    Create encryption.py

---
+++ encryption.py
@@ -0,0 +1,64 @@
+
+from marshal import dumps
+import os, random, zlib, lzma
+def purplepink(text):
+    os.system(""); faded = ""
+    red = 40
+    for line in text.splitlines():
+        faded += (f"\033[38;2;{red};0;220m{line}\033[0m\n")
+        if not red == 255:
+            red += 15
+            if red > 255:
+                red = 255
+    return faded
+junk = "__thejatinkalwar__" * 15
+
+
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+
+def genvar():
+    var = ''
+    for i in range(10):
+        var += random.choice(chars)
+    return var
+
+def compress(text):
+    ok=zlib.compress(text.encode())
+    ok=lzma.compress(ok)
+    return ok
+
+def encrypt1(text):
+    src = compile(text, 'coduter', 'exec')
+    ma = dumps(src)
+    s = f'{junk}="{junk}";{junk}="{junk}";{junk}="{junk}";exec(loads({ma}));{junk}="{junk}";{junk}="{junk}"'
+    compresse = compress(s)
+    oke = f"import zlib,lzma\nexec(zlib.decompress(lzma.decompress({compresse})))"
+    return oke
+
+def encrypt2(text):
+    sta = genvar()
+    code = text
+    s = compile(code, 'coduter', 'exec')
+    maa = dumps(s)
+    stub2 = f'from marshal import loads;exec(loads({maa}));'
+    fin = f'{junk}="{junk}";{junk}="{junk}";{stub2}{junk}="{junk}";{junk}="{junk}";'
+    return fin
+
+
+def main(file):
+    if not os.path.isfile(file):
+        print('File not found')
+        exit()
+    code = open(file, 'r').read()
+    code = encrypt1(code)
+    code = encrypt2(code) 
+    print('\n')
+    name = file.split('/')[-1]
+    name = name.split('.')[0]
+    with open(f'{name}-ch.py', 'w') as f:
+        f.write(code)
+    import time
+    exit(1337)
+
