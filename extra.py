9c76a7fa3f8866a4b6d85da4f3e38d0d06f8ad09
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-07-27 22:33:56 +0530

    Update extra.py

--- extra.py
+++ extra.py
@@ -141,12 +141,8 @@ async def downloadindsms(tarnum, skey):
     os.system("rm indisms.py bomber-indisms.py")
     copyfile("indiaa.py", "indisms.py")
     file = "indisms.py"
-    os.system("ls")
-    print("hi")
     os.system(f"sed -i s/€tor/{tarnum}/g indisms.py")
-    print("hi")
     await setup(file)
-    print("hi")
     return "ok"
 
 
