087a1867e6f18744f12466aa2e23d76e1108a78d
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-07-27 15:38:16 +0530

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -2,102 +2,106 @@ import base64, codecs, random, os, zlib
 
 
 async def obfuscation(file):
-    bruh = codecs.encode(file)
-    a = base64.b64encode(bruh)
-    b = codecs.decode(a)
-    c = r"exec(codecs.decode(base64.b64decode(r'" + b + "')))"
-
-    bruh = codecs.encode(c)
-    d = base64.b32encode(bruh)
-    e = codecs.decode(d)
-    f = r"exec(codecs.decode(base64.b32decode(r'" + e + "')))"
-
-    bruh = codecs.encode(f)
-    g = base64.b16encode(bruh)
-    h = codecs.decode(g)
-    ch = r"exec(codecs.decode(base64.b16decode(r'" + h + "')))"
-
-    bruh = codecs.encode(ch)
-    i = base64.b85encode(bruh)
-    j = codecs.decode(i)
-    k = r"exec(codecs.decode(base64.b85decode(r'" + j + "')))"
-
-    ############################################################################ LOOPS
-
-    bruh = codecs.encode(k)
-    a = base64.b64encode(bruh)
-    b = codecs.decode(a)
-    c = r"exec(codecs.decode(base64.b64decode(r'" + b + "')))"
-
-    bruh = codecs.encode(c)
-    d = base64.b32encode(bruh)
-    e = codecs.decode(d)
-    f = r"exec(codecs.decode(base64.b32decode(r'" + e + "')))"
-
-    bruh = codecs.encode(f)
-    g = base64.b16encode(bruh)
-    h = codecs.decode(g)
-    ch = r"exec(codecs.decode(base64.b16decode(r'" + h + "')))"
-
-    bruh = codecs.encode(ch)
-    i = base64.b85encode(bruh)
-    j = codecs.decode(i)
-    k = r"exec(codecs.decode(base64.b85decode(r'" + j + "')))"
-
-    bruh = codecs.encode(k)
-    a = base64.b64encode(bruh)
-    b = codecs.decode(a)
-    c = r"exec(codecs.decode(base64.b64decode(r'" + b + "')))"
-
-    bruh = codecs.encode(c)
-    d = base64.b32encode(bruh)
-    e = codecs.decode(d)
-    f = r"exec(codecs.decode(base64.b32decode(r'" + e + "')))"
-
-    bruh = codecs.encode(f)
-    g = base64.b16encode(bruh)
-    h = codecs.decode(g)
-    ch = r"exec(codecs.decode(base64.b16decode(r'" + h + "')))"
-
-    bruh = codecs.encode(ch)
-    i = base64.b85encode(bruh)
-    j = codecs.decode(i)
-    k = r"exec(codecs.decode(base64.b85decode(r'" + j + "')))"
-
-    bruh = codecs.encode(k)
-    a = str(zlib.compress(bruh, 9))
-    c = r"exec(codecs.decode(zlib.decompress(" + "bytes(" + a + ")" + ")))"
-
-    W = imports + imports + c + imports
-
     try:
-        creationoffile = open(curdirectory + "\\" + "bomber_" + file_to_obfuscatestr, "x")
+        bruh = codecs.encode(file)
+        a = base64.b64encode(bruh)
+        b = codecs.decode(a)
+        c = r"exec(codecs.decode(base64.b64decode(r'" + b + "')))"
+    
+        bruh = codecs.encode(c)
+        d = base64.b32encode(bruh)
+        e = codecs.decode(d)
+        f = r"exec(codecs.decode(base64.b32decode(r'" + e + "')))"
+    
+        bruh = codecs.encode(f)
+        g = base64.b16encode(bruh)
+        h = codecs.decode(g)
+        ch = r"exec(codecs.decode(base64.b16decode(r'" + h + "')))"
+    
+        bruh = codecs.encode(ch)
+        i = base64.b85encode(bruh)
+        j = codecs.decode(i)
+        k = r"exec(codecs.decode(base64.b85decode(r'" + j + "')))"
+    
+        ############################################################################ LOOPS
+    
+        bruh = codecs.encode(k)
+        a = base64.b64encode(bruh)
+        b = codecs.decode(a)
+        c = r"exec(codecs.decode(base64.b64decode(r'" + b + "')))"
+    
+        bruh = codecs.encode(c)
+        d = base64.b32encode(bruh)
+        e = codecs.decode(d)
+        f = r"exec(codecs.decode(base64.b32decode(r'" + e + "')))"
+    
+        bruh = codecs.encode(f)
+        g = base64.b16encode(bruh)
+        h = codecs.decode(g)
+        ch = r"exec(codecs.decode(base64.b16decode(r'" + h + "')))"
+    
+        bruh = codecs.encode(ch)
+        i = base64.b85encode(bruh)
+        j = codecs.decode(i)
+        k = r"exec(codecs.decode(base64.b85decode(r'" + j + "')))"
+    
+        bruh = codecs.encode(k)
+        a = base64.b64encode(bruh)
+        b = codecs.decode(a)
+        c = r"exec(codecs.decode(base64.b64decode(r'" + b + "')))"
+    
+        bruh = codecs.encode(c)
+        d = base64.b32encode(bruh)
+        e = codecs.decode(d)
+        f = r"exec(codecs.decode(base64.b32decode(r'" + e + "')))"
+    
+        bruh = codecs.encode(f)
+        g = base64.b16encode(bruh)
+        h = codecs.decode(g)
+        ch = r"exec(codecs.decode(base64.b16decode(r'" + h + "')))"
+    
+        bruh = codecs.encode(ch)
+        i = base64.b85encode(bruh)
+        j = codecs.decode(i)
+        k = r"exec(codecs.decode(base64.b85decode(r'" + j + "')))"
+    
+        bruh = codecs.encode(k)
+        a = str(zlib.compress(bruh, 9))
+        c = r"exec(codecs.decode(zlib.decompress(" + "bytes(" + a + ")" + ")))"
+    
+        W = imports + imports + c + imports
+    
+        try:
+            creationoffile = open(curdirectory + "\\" + "bomber_" + file_to_obfuscatestr, "x")
+            creationoffile.close()
+        except Exception:
+            pass
+    
+        creationoffile = open(curdirectory + "\\" + "bomber_" + file_to_obfuscatestr, "w")
+        creationoffile.write(W)
         creationoffile.close()
-    except Exception:
-        pass
-
-    creationoffile = open(curdirectory + "\\" + "bomber_" + file_to_obfuscatestr, "w")
-    creationoffile.write(W)
-    creationoffile.close()
+    except Exception as e:
+        print(e)
 
 
 async def setup(filee: str):
-    global file_to_obfuscatestr
-    global curdirectory
-    global a
-    global imports
-    global prepped
-
-    file_to_obfuscatestr = filee
-
-    curdirectory = os.path.dirname(os.path.abspath(__file__))
-
-    file_to_obfuscate_open = open(curdirectory + "\\" + file_to_obfuscatestr, "r")
-    a = str(file_to_obfuscate_open.read())
-    file_to_obfuscate_open.close()
-    prepped = str(a.strip())
-
-    imports = """
+    try:
+        global file_to_obfuscatestr
+        global curdirectory
+        global a
+        global imports
+        global prepped
+    
+        file_to_obfuscatestr = filee
+    
+        curdirectory = os.path.dirname(os.path.abspath(__file__))
+    
+        file_to_obfuscate_open = open(curdirectory + "\\" + file_to_obfuscatestr, "r")
+        a = str(file_to_obfuscate_open.read())
+        file_to_obfuscate_open.close()
+        prepped = str(a.strip())
+    
+        imports = """
 # 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	
 # 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	
 # 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	 	 	 CODED BY JATIN KALWAR (YOURHACKINGHOUSE TEAM)	 	
@@ -175,7 +179,9 @@ import zlib
 import codecs
 import base64
 """
-    await obfuscation(prepped)
+        await obfuscation(prepped)
+    except Exception as e:
+        print(e)
 
 
 
